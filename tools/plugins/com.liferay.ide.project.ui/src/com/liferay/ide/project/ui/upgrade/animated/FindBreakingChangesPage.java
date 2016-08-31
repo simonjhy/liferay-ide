e72f9afd7db7787976602378799a96052b91730e
Andy Wu <andy.wu@liferay.com>
2016-08-31 15:52:14 +0800

    add find bc page

--- tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/animated/FindBreakingChangesPage.java
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/animated/FindBreakingChangesPage.java
@@ -14,27 +14,64 @@
  *******************************************************************************/
 package com.liferay.ide.project.ui.upgrade.animated;
 
+import com.liferay.ide.project.ui.migration.MigrationView;
+import com.liferay.ide.project.ui.migration.RunMigrationToolAction;
+import com.liferay.ide.ui.util.UIUtil;
+
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 /**
- * @author Adny
+ * @author Andy Wu
  * @author Simon Jiang
  */
 public class FindBreakingChangesPage extends Page
 {
     PageAction[] actions = { new PageFinishAction(), new PageSkipAction() };
-    
+
     public FindBreakingChangesPage( Composite parent, int style, LiferayUpgradeDataModel dataModel )
     {
         super( parent, style,dataModel );
-        
-        this.setLayout( new FillLayout() );
-        
-        Button button = new Button(this, SWT.PUSH);
-        button.setText( "Find Breaking Changes" );
+
+        GridLayout layout = new GridLayout( 1, true );
+        this.setLayout( layout );
+
+        Label title = new Label( this, SWT.LEFT );
+        title.setText( "Find Breaking Changes" );
+        title.setFont( new Font( null, "Times New Roman", 16, SWT.NORMAL ) );
+
+        Text content = new Text( this, SWT.MULTI );
+        final String descriptor =
+                        "This step will help you to find  breaking changes for type of java , jsp , xml " +
+                        "and properties file.\n" +
+                        "It  will not support to find the front-end codes( e.g., javascript, css).\n" +
+                        "For service builder, you just need to modify the changes on xxxServiceImp.class, " +
+                        "xxxFinder.class, xxxModel.class.\n" +
+                        "Others will be solved at step ¡°Build Service¡±. ";
+
+        content.setText( descriptor );
+
+        Button findBCButton = new Button(this, SWT.PUSH);
+
+        findBCButton.setText( "Find Breaking Changes" );
+
+        findBCButton.addSelectionListener( new SelectionAdapter()
+        {
+            @Override
+            public void widgetSelected( SelectionEvent e )
+            {
+                MigrationView view = (MigrationView) UIUtil.showView( MigrationView.ID );
+
+                new RunMigrationToolAction( "Run Migration Tool", view.getViewSite().getShell() ).run();
+            }
+        } );
 
         setActions( actions );
         this.setPageId( FINDBREACKINGCHANGES_PAGE_ID );
