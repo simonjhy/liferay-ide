7289a00890f3f99c9c8ab1a93dc45706f36c912f
Simon Jiang <simon.jiang@liferay.com>
2016-11-03 14:44:26 +0800

    remove build-css menu for maven moudle project

--- maven/plugins/com.liferay.ide.maven.ui/src/com/liferay/ide/maven/ui/MavenProjectPropertyTester.java
+++ maven/plugins/com.liferay.ide.maven.ui/src/com/liferay/ide/maven/ui/MavenProjectPropertyTester.java
@@ -14,32 +14,66 @@
  *******************************************************************************/
 package com.liferay.ide.maven.ui;
 
-import com.liferay.ide.maven.core.MavenUtil;
-
+import org.apache.maven.model.Plugin;
+import org.apache.maven.project.MavenProject;
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.internal.IMavenConstants;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+
+import com.liferay.ide.core.IBundleProject;
+import com.liferay.ide.core.ILiferayProject;
+import com.liferay.ide.core.LiferayCore;
+import com.liferay.ide.maven.core.ILiferayMavenConstants;
+import com.liferay.ide.maven.core.MavenUtil;
 
 
 /**
  * @author Gregory Amerson
  */
+@SuppressWarnings("restriction")
 public class MavenProjectPropertyTester extends PropertyTester
 {
 
     public boolean test( Object receiver, String property, Object[] args, Object expectedValue )
     {
-        if( receiver instanceof IProject )
-        {
-            try
-            {
-                return MavenUtil.isMavenProject( (IProject) receiver );
-            }
-            catch( CoreException e )
-            {
-                // don't log error
-            }
-        }
+		if (receiver instanceof IProject)
+		{
+			try
+			{
+				IProject project = (IProject) receiver;
+				boolean isMavenProject = MavenUtil.isMavenProject(project);
+
+				if (isMavenProject == true)
+				{
+					IMavenProjectFacade projectFacade = MavenUtil.getProjectFacade(project, new NullProgressMonitor());
+
+					if ( projectFacade != null )
+					{
+						MavenProject mavenProject = projectFacade.getMavenProject();
+
+						final Plugin liferayMavenplugin = mavenProject.getPlugin(ILiferayMavenConstants.LIFERAY_MAVEN_PLUGIN_KEY);
+
+						if (liferayMavenplugin != null)
+						{
+							return true;
+						}
+					}
+
+					return false;
+				}
+
+				return false;
+			}
+			catch (CoreException e)
+			{
+				// don't log error
+			}
+		}
 
         return false;
     }
